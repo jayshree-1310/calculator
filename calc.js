a50c7134c0420c074f1aabedc64e50d5460d5c07
Jayshree Upadhyay <85044831+jayshree-1310@users.noreply.github.com>
2022-09-15 13:58:49 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,22 @@
+let screen=document.getElementById('screen');
+buttons=document.querySelectorAll('button');
+let screenValue= '';
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText=e.target.innerText;
+        console.log(buttonText);
+        if (buttonText=="AC"){
+            screenValue="";
+            screen.value=screenValue;
+        }
+    
+        
+        else if(buttonText=='='){
+            screen.value=eval(screenValue);
+        }
+        else{
+            screenValue += buttonText;
+            screen.value=screenValue;
+        }
+    })
+}
